9820ec60167adbf40549f7a47cc79b40ac4ec0fc
Adam Hoelscher <you@example.com>
2017-06-19 01:02:55 -0700

    Working problem 73

--- Problem73.py
+++ Problem73.py
@@ -1,27 +1,52 @@
 from Functions import GCD
-from fractions import Fraction
+from math import ceil
 
 def Solve(limit=12000, verbose=False):
 
     count = 0
-    band = set()
 
     for d in range(2, limit+1):
-        if verbose: print(d)
-        low = d//3
-        high = d//2 + 1
+        if verbose and not d % 100: print(d)
+        low = d//3 + 1
+        high = ceil(d/2)
         for n in range(low, high):
-            f = Fraction(n, d)
-            if Fraction(1,3) < f < Fraction(1,2):
-                band.add(f)
+            if GCD(n, d) == 1:
+                if verbose: print(n,d)
+                count += 1
 
+    return count
 
-    return len(band)
+def Solve2(limit=12000, verbose=False):
+
+    composites = {}
+
+    count = 0
+
+    for n in range(2, limit+1):
+        if n not in composites:
+            composites[n] = [n]
+        for p in composites[n]:
+            composites.setdefault(p + n, []).append(p)
+
+        adj = 0
+
+        totient = n
+        for x in composites[n]:
+            totient //= x
+            totient *= (x-1)
+            if n//3 + 1 > x or x > ceil(n/2):
+                adj += 1
+
+        count += totient - adj
+
+        del composites[n]
+
+    return count
 
 if __name__=='__main__':
     import cProfile
     limit = 12000
-    # limit = 8
+    limit = 8
     verbose = True
-    # verbose = False
-    cProfile.run('print(Solve(' + str(limit) + ',' + str(verbose) + '))')
+    verbose = False
+    cProfile.run('print(Solve2(' + str(limit) + ',' + str(verbose) + '))')
