61755263c261e7395cf61280de56562903e49fec
Adam Hoelscher <you@example.com>
2017-09-10 14:00:11 -0700

    Changed problem 14 to use a dict() object to cache answers

--- Problem14.py
+++ Problem14.py
@@ -1,29 +1,58 @@
-def Solve():
+def Solve(to = 10**6):
 
-    lengths = {}
-    maxLen = 0
-    maxKey = 0
-
+    # replaced original function with a version that uses a dict
+    # improved from 3.9 seconds to 2.7
     def Col(n):
-        if n%2 == 0:
-            return(n/2)
+        if n%2:
+            return (3 * n + 1)
         else:
-            return(3*n + 1)
+            return(n/2)
 
-    for i in range(1, 1000000):
-        n = i
-        count = 0
-        stop = False
-        while (n != 1):
-            try:
-                count += lengths[n]
-                break
-            except:
-                n = Col(n)
-                count += 1
-        lengths[i] = count
-        if count > maxLen:
-            maxLen = count
-            maxKey = i
+    lengths = {1:1}
 
-    return(maxKey)
+    def FindLength(x):
+        l = lengths.get(x, 0)
+        if l:
+            temp = l
+        else:
+            NextColl = Col(x)
+            temp = 1 + FindLength(NextColl)
+            lengths[x] = temp
+        return(temp)
+
+    temp = [FindLength(x) for x in range(1, to, 2)]
+    return(temp.index(max(temp)))
+
+# def Solve(to = 10**6):
+#
+#     lengths = {}
+#     maxLen = 0
+#     maxKey = 0
+#
+#     def Col(n):
+#         if n%2:
+#             return (3 * n + 1)
+#         else:
+#             return(n/2)
+#
+#     for i in range(1, to):
+#         n = i
+#         count = 0
+#         stop = False
+#         while (n != 1):
+#             try:
+#                 count += lengths[n]
+#                 break
+#             except:
+#                 n = Col(n)
+#                 count += 1
+#         lengths[i] = count
+#         if count > maxLen:
+#             maxLen = count
+#             maxKey = i
+#
+#     return(maxKey)
+
+if __name__ == '__main__':
+    import cProfile
+    cProfile.run('Solve()')
