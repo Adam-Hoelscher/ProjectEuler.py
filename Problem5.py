f58a04d6965f2ce15f17d13bebddc1b12bc84821
Adam Hoelscher <adam.hoelscher@gmail.com>
2020-03-29 01:26:06 -0700

    pythonize

--- Problem5.py
+++ Problem5.py
@@ -2,13 +2,10 @@ def Solve():
     p = 1
     for c in range(1, 21):
         i = p
-        while (p % c != 0):
+        while p % c:
             p += i
-    return(p)
+    return p
 
-def Solve2():
-    from Functions import LCM
-    return(LCM(range(1,21)))
 
 if __name__ == '__main__':
     print(Solve())
