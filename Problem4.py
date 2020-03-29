ed8fa182abb41f381f0fd8d65bccc15ddc62873d
Adam Hoelscher <adam.hoelscher@gmail.com>
2020-03-29 01:25:29 -0700

    break the inner loop for optimization

--- Problem4.py
+++ Problem4.py
@@ -2,11 +2,14 @@ def Solve():
     temp = 0
     for x in range(999, 99, -1):
         for y in range(990, 99, -11):
-            test = x*y
-            if (test > temp):
+            test = x * y
+            if test > temp:
                 string = str(test)
-                if (string==string[::-1]):
+                if string == string[::-1]:
                     temp = test
+            else:
+                break
+    return temp
 
 
 if __name__ == '__main__':
