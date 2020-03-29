179415be210cb0d1aaeca3531eceb2d591611dc5
Adam Hoelscher <adam.hoelscher@gmail.com>
2020-03-29 01:27:14 -0700

    optimize

--- Problem35.py
+++ Problem35.py
@@ -3,33 +3,42 @@ from Functions import PrimeSieve
 def Solve():
 
     limit = 1000000
-    primes = [x for x in PrimeSieve(limit)]
-    CircPrimes = []
+    primes = set(PrimeSieve(limit))
+    CircPrimes = set()
 
-    for n in primes:
+    for n in set(primes):
         if n >= 10:
-            digs = [int(x) for x in str(n)]
-            evens = [z for z in digs if z % 2 == 0]
-            if len(evens) > 0:
+            digits = set(int(x) for x in str(n))
+            if digits.intersection(range(0, 10, 2)):
                 primes.remove(n)
-            elif 5 in digs:
+            elif 5 in digits:
                 primes.remove(n)
 
     def rotate(n):
-        z = [x for x in str(n)]
-        z = z[1:] + [z[0]]
+        z = list(str(n))
+        z = z[1:] + z[:1]
         return(int(''.join(z)))
 
-    for i in [x for x in primes]:
-        n = i
-        if i in primes:
-            temp = []
-            while n in primes:
-                primes.remove(n)
-                temp.append(n)
-                n = rotate(n)
-            if n == i:
-                CircPrimes = CircPrimes + temp
+    for i in primes:
+
+        if i in CircPrimes:
+            continue
+
+        else:
+
+            n = i
+            temp = set()
+
+            while n not in temp:
+
+                if n not in primes:
+                    break
+                else:
+                    temp.add(n)
+                    n = rotate(n)
+
+            else:
+                CircPrimes.update(temp)
 
     return(len(CircPrimes))
 
