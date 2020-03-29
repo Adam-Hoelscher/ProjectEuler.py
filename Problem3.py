16eed46960c5d450353021ebbf9c09c0a8209495
Adam Hoelscher <adam.hoelscher@gmail.com>
2020-03-29 01:23:21 -0700

    pythonize

--- Problem3.py
+++ Problem3.py
@@ -1,9 +1,15 @@
-def Solve():
-    p = 600851475143
-    f = 2
-    while (p != 1):
-        if (p % f == 0):
+from Functions import PrimeSieve
+
+def Solve(p=600851475143):
+    prime = PrimeSieve()
+    f = next(prime)
+    while not p == 1:
+        if p % f == 0:
             p = p / f
         else:
+            f = next(prime)
+    return f
+
+
 if __name__ == '__main__':
     print(Solve())
