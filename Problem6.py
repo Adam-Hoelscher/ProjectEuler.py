66d16d5c2077c54cb7830b2b80f92630ac03caa2
Adam Hoelscher <adam.hoelscher@gmail.com>
2020-03-29 01:26:35 -0700

    optimize

--- Problem6.py
+++ Problem6.py
@@ -1,2 +1,8 @@
+def Solve(limit=100):
+    sq_sum = sum(range(limit + 1)) ** 2
+    sum_sq = sum(x ** 2 for x in range(limit + 1))
+    return sq_sum - sum_sq
+
+
 if __name__ == '__main__':
     print(Solve())
